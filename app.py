c29ddcba952394b986d05b009714a9403d128d1d
Siyamanga malawu <60347148+mlwsiy005@users.noreply.github.com>
2020-06-27 06:16:41 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -30,6 +30,8 @@ import pickle
 # Data dependencies
 import pandas as pd
 
+nltk.download()
+
 # nltk
 from nltk.tokenize import word_tokenize, TreebankWordTokenizer
 from nltk.corpus import stopwords
@@ -50,7 +52,7 @@ pattern_url = r'http[s]?://(?:[A-Za-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9A-Fa-f]
 subs_url = r'url-web'
 train['message'] = train['message'] .replace(to_replace = pattern_url, value = subs_url, regex = True)
 #test['message'] = test['message'] .replace(to_replace = pattern_url, value = subs_url, regex = True)
-nltk.download('stopwords')
+
 stop_words = set(stopwords.words("english")) 
 lemmatizer = WordNetLemmatizer()
 
